a9d4cdadafc4789af3632686611d04813ce9811f
Aniket Dhakane <aniketdhakane3@gmail.com>
2024-12-24 23:21:40 +0530

    added database schema

---
+++ db.js
@@ -0,0 +1,44 @@
+const { Schema, default: mongoose } = require('mongoose');
+require('dotenv').config()
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING);
+
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+    email: { type : String, unique : true},
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+})
+
+const adminSchema = new Schema({
+    email: { type : String, unique : true},
+    password: String,
+    firstName: String,
+    lastName: String
+})
+
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    courseId: ObjectId    
+})
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// const mongoose = require('mongoose');
 const express = require('express');
 const { userRouter } = require('./routes/user');
 const { courseRouter } = require('./routes/course');
-const { adminRouter } = require('./routes/admin ')
+const { adminRouter } = require('./routes/admin');
 const app = express();
+
+
 
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/admin', adminRouter);
 app.use('/api/v1/course', courseRouter);
-
 
 
 app.listen(3000, () => {
