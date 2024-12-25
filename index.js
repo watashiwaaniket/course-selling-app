a23fb61a2b1227eb5a5bacfc32972a2845176590
Aniket Dhakane <aniketdhakane3@gmail.com>
2024-12-25 23:00:44 +0530

    added signup and signin endpoints in the admin routes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { userRouter } = require('./routes/user');
-const { courseRouter } = require('./routes/course');
-const { adminRouter } = require('./routes/admin');
+const { userRouter } = require('./routes/user.routes');
+const { courseRouter } = require('./routes/course.routes');
+const { adminRouter } = require('./routes/admin.routes');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config()
---
+++ routes/admin.routes.js
@@ -0,0 +1,69 @@
+const { Router } = require('express');
+const adminRouter = Router();
+const { adminModel } = require('../db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+adminRouter.post('/signup',async function(req, res){
+    const { email, password, firstName, lastName } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 5);
+
+    await adminModel.create({
+        email: email,
+        password: hashedPassword,
+        firstName: firstName,
+        lastName: lastName       
+    });
+
+    res.json({
+        message: "You are signed up!"
+    })
+
+});
+
+adminRouter.post('/signin',async function(req, res){
+    const { email, password, firstName, lastName } = req.body;
+    const response = await adminModel.findOne({
+        email: email,
+        firstName: firstName,
+        lastName: lastName
+    })
+
+    if(bcrypt.compareSync(password, response.password.toString())){
+        const token = jwt.sign({
+            id: response._id.toString()
+        }, process.env.JWT_ADMIN_PASSWORD);
+
+        res.json({
+            token
+        })
+    }else{
+        res.status(403).json({
+            message: "Incorrect Creds"
+        })
+    }
+});
+
+adminRouter.post('/course', function(req, res){
+    res.json({
+        message: "course post endpoint"
+    })
+})
+
+adminRouter.put('/course', function(req, res){
+    res.json({
+        message: "course put endpoint"
+    })
+})
+
+adminRouter.get('/course/bulk', function(req, res){
+    res.json({
+        message: "course get endpoint"
+    })
+})
+
+module.exports = {
+    adminRouter: adminRouter
+}
